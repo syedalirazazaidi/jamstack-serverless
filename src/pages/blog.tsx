cfd4aa09752538d25fab65aca868aa161b61b2b3
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-22 22:09:32 +0500

    module.css

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,16 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
 import "../components/layout.css"
+
+import styles from "../components/blog.module.css"
 export default function blog() {
   return (
     <Layout>
-      <h1>This is Blog Page</h1>
-      <p className="blog-text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis magni
-        autem exercitationem earum excepturi placeat blanditiis voluptatibus
-        illo voluptate consectetur quidem eveniet expedita magnam dolorum
-        voluptates ex quod, consequuntur eius!
-      </p>
+      <div className={styles.page}>
+        <h1>This is Blog Page</h1>
+        <p className="blog-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis magni
+          autem exercitationem earum excepturi placeat blanditiis voluptatibus
+          illo voluptate consectetur quidem eveniet expedita magnam dolorum
+          voluptates ex quod, consequuntur eius!
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,16 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
 import "../components/layout.css"
+import styles from "../components/products.module.css"
 export default function products() {
   return (
     <Layout>
-      <h1>Our Products</h1>
-      <p className="products-text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis magni
-        autem exercitationem earum excepturi placeat blanditiis voluptatibus
-        illo voluptate consectetur quidem eveniet expedita magnam dolorum
-        voluptates ex quod, consequuntur eius!
-      </p>
+      <div className={styles.page}>
+        <h1>Our Products</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis magni
+          autem exercitationem earum excepturi placeat blanditiis voluptatibus
+          illo voluptate consectetur quidem eveniet expedita magnam dolorum
+          voluptates ex quod, consequuntur eius!
+        </p>
+      </div>
     </Layout>
   )
 }
