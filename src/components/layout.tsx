19e9e89509ada7a96d838d1494cbc6a50fdf47f0
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-22 12:43:59 +0500

    adding css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react"
 import Footer from "./footer"
 import Navbar from "./navbar"
+import "./layout.css"
 type LayoutProps = {
   children: ReactNode
 }
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,6 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import "../components/layout.css"
 export default function products() {
-  return <Layout>Our Products</Layout>
+  return (
+    <Layout>
+      <h1>Our Products</h1>
+      <p className="products-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis magni
+        autem exercitationem earum excepturi placeat blanditiis voluptatibus
+        illo voluptate consectetur quidem eveniet expedita magnam dolorum
+        voluptates ex quod, consequuntur eius!
+      </p>
+    </Layout>
+  )
 }
