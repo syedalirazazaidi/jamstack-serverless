dbcdaba34637bf06f736a890c28190a220f53719
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-22 10:12:50 +0500

    starter project

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Link } from "gatsby"
+export default function About() {
+  return (
+    <div>
+      <Link to="/">Back to Home</Link>
+      about my Home Page
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Link } from "gatsby"
+export default function Home() {
+  return (
+    <div>
+      <Link to="/about">Back to About Page</Link>
+      about my page!
+    </div>
+  )
+}
