3a59ea847397e1d98b1ec663beebb68089b8ceed
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-22 23:29:12 +0500

    styles-component

---
+++ src/components/button.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+interface Boxfrom {
+  theme?: ThemeInterface
+  borders?: boolean
+  className?: string
+}
+
+export const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Navbar from "../components/navbar"
 import Layout from "../components/layout"
+import { Button } from "../components/button"
 export default function Home() {
   return (
     <Layout>
       <h1>hello from gatsby</h1>
+      <Button>Click Me</Button>
     </Layout>
   )
 }
