3cf4d1b18511ee94768bed4fa0af388cb7fd4a5a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-12-24 12:16:10 +0500

    add nested pages

---
+++ src/pages/products/latest.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Latest() {
+  return (
+    <div>
+      Hello from latest product
+      <p>and the url is product latest</p>
+    </div>
+  )
+}
